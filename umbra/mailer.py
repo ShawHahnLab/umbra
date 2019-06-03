0b97bf15786e8279a401e70e8005ef240435af3b
Jesse Connell <ancon@upenn.edu>
2019-06-03 11:04:49 -0400

    lint fixes for mailer
    Switch to dict-based configuration for Mailer objects.

--- umbra/mailer.py
+++ umbra/mailer.py
@@ -10,6 +10,7 @@ import socket
 import smtplib
 import os
 import logging
+import copy
 from email.message import EmailMessage
 
 # https://stackoverflow.com/a/2899055
@@ -18,6 +19,8 @@ def get_username():
     entry = pwd.getpwuid(os.getuid())
     username = entry[0]
     return username
+
+LOGGER = logging.getLogger(__name__)
 
 class Mailer:
     """A simple interface for sending email.
@@ -31,36 +34,23 @@ class Mailer:
     # (What's wrong with just having one public method?  A Mailer instance
     # mails.  That's it.)
 
-    def __init__(
-            self, host="localhost", port=25,
-            use_ssl=False, auth=False, user=None, password=None, from_addr=None,
-            cc_addrs=None, reply_to=None, **kwargs):
+    def __init__(self, conf):
         """Configure connection details for sending mail over SMTP."""
-        self.logger = logging.getLogger(__name__)
-        self.host = host
-        self.port = port
-        self.ssl = use_ssl
-        self.auth = auth
-        self.__user = user
-        self.__password = password
-        self.from_addr = from_addr
-        self.reply_to = reply_to
-        self.kwargs = kwargs
-        if isinstance(cc_addrs, str):
-            cc_addrs = [cc_addrs]
-        if not cc_addrs:
-            cc_addrs = []
-        self.cc_addrs = cc_addrs
-        self.logger.debug("Mailer initialized.")
+        self.conf = copy.deepcopy(conf)
+        if isinstance(self.conf.get("cc_addrs"), str):
+            self.conf["cc_addrs"] = [self.conf.get("cc_addrs")]
+        if not self.conf.get("cc_addrs"):
+            self.conf["cc_addrs"] = []
+        LOGGER.debug("Mailer initialized.")
 
     def _resolve_from_addr(self, addr, addr_default):
         addr = addr or addr_default
         if not addr:
-            name = self.__user or get_username()
+            name = self.conf.get("user") or get_username()
             if "@" in name:
                 addr = name
             else:
-                srv = self.host
+                srv = self.conf.get("host")
                 if socket.getfqdn(srv) == "localhost":
                     srv = socket.getfqdn()
                 addr = name + "@" + srv
@@ -72,13 +62,14 @@ class Mailer:
 
         This will connect to the SMTP server (with authentication if enabled
         for the object), format and send a single message, and disconnect."""
-        self.logger.debug('Preparing message: "%s"', subject)
+        # pylint: disable=too-many-arguments
+        LOGGER.debug('Preparing message: "%s"', subject)
         if isinstance(to_addrs, str):
             to_addrs = [to_addrs]
         # From address can be set already or given here.  Either way, if it was
         # not defined, construct a From address from user and network details.
-        from_addr = self._resolve_from_addr(from_addr, self.from_addr)
-        reply_to = reply_to or self.reply_to
+        from_addr = self._resolve_from_addr(from_addr, self.conf.get("from_addr"))
+        reply_to = reply_to or self.conf.get("reply_to")
         # Construct message
         msg = EmailMessage()
         msg["Subject"] = subject
@@ -87,9 +78,9 @@ class Mailer:
             msg["Reply-To"] = reply_to
         if to_addrs:
             msg["To"] = ", ".join(to_addrs)
-        self.logger.debug('Connecting over SMTP for message: "%s"', subject)
-        if self.cc_addrs:
-            msg["CC"] = ", ".join(self.cc_addrs)
+        LOGGER.debug('Connecting over SMTP for message: "%s"', subject)
+        if self.conf.get("cc_addrs"):
+            msg["CC"] = ", ".join(self.conf.get("cc_addrs"))
         if msg_html:
             msg.set_type("multipart/alternative")
             msg1 = EmailMessage()
@@ -101,25 +92,25 @@ class Mailer:
             msg.set_payload([msg1, msg2])
         else:
             msg.set_payload(msg_body)
-        recipients = to_addrs + self.cc_addrs
+        recipients = to_addrs + self.conf.get("cc_addrs")
         # If there are no receipients, don't actually try to send.  If there
         # are recipients but no "To:" addresses, just warn.
         if not recipients:
-            self.logger.error(
+            LOGGER.error(
                 'No receipients given, skipping message: "%s"', subject)
             return msg
         if not to_addrs:
-            self.logger.warning(
+            LOGGER.warning(
                 'No "To:" addresses given for message: "%s"', subject)
-        self.logger.debug('Connecting over SMTP for message: "%s"', subject)
-        with smtplib.SMTP(self.host, port=self.port) as smtp:
-            if self.ssl:
+        LOGGER.debug('Connecting over SMTP for message: "%s"', subject)
+        with smtplib.SMTP(self.conf.get("host"), port=self.conf.get("port")) as smtp:
+            if self.conf.get("ssl"):
                 context = ssl.create_default_context()
                 smtp.starttls(context=context)
-            if self.auth:
-                smtp.login(self.__user, self.__password)
+            if self.conf.get("auth"):
+                smtp.login(self.conf.get("user"), self.conf.get("password"))
             # Send and quit
-            self.logger.debug('Sending message: "%s"', subject)
+            LOGGER.debug('Sending message: "%s"', subject)
             # "Note: The from_addr and to_addrs parameters are used to construct
             # the message envelope used by the transport agents. sendmail does
             # not modify the message headers in any way."
@@ -127,5 +118,5 @@ class Mailer:
             # inside the msg object, even though we're also giving it
             # explicitly to smtp.sendmail.
             smtp.sendmail(from_addr, recipients, msg.as_string())
-        self.logger.info('Message sent: "%s"', subject)
+        LOGGER.info('Message sent: "%s"', subject)
         return msg
