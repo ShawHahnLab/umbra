df23e18ca71162006041f8970fa2af9b54610050
Jesse Connell <ancon@upenn.edu>
2020-08-03 14:37:26 -0400

    For #71: failing tests for metadata.csv edge cases

---
+++ test_umbra/test_experiment.py
@@ -0,0 +1,56 @@
+"""
+Tests for experiment metadata handlers.
+"""
+
+from collections import OrderedDict
+from umbra import experiment
+from .test_common import TestBase
+
+class TestLoadMetadata(TestBase):
+    """Test basic metadata loading.
+
+    Each version of metadata.csv should provide the same list of
+    dictionaries.
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.exp_path = self.paths["exp"] / "Partials_1_1_18"
+        self.expected = [ \
+            OrderedDict([
+                ('Sample_Name', '1086S1_01'), ('Project', 'STR'),
+                ('Contacts', {'Jesse': 'ancon@upenn.edu'}), ('Tasks', ['trim'])]),
+            OrderedDict([
+                ('Sample_Name', '1086S1_02'), ('Project', 'STR'),
+                ('Contacts', {'Jesse': 'ancon@upenn.edu'}), ('Tasks', ['trim'])]),
+            OrderedDict([
+                ('Sample_Name', '1086S1_03'), ('Project', 'Something Else'),
+                ('Contacts', {'Someone': 'person@gmail.com'}), ('Tasks', [])]),
+            OrderedDict([
+                ('Sample_Name', '1086S1_04'), ('Project', 'Something Else'),
+                ('Contacts', {'Someone': 'person@gmail.com', 'Jesse Connell': 'ancon@upenn.edu'}),
+                ('Tasks', [])])]
+
+    def test_load_metadata(self):
+        """Test basic metadata loading."""
+        fp_metadata = self.exp_path / "metadata.csv"
+        info = experiment.load_metadata(fp_metadata)
+        self.assertEqual(self.expected, info)
+
+    def test_load_metadata_emptyrows(self):
+        """Test metadata loading including empty rows.
+
+        Empty rows should be silently removed.
+        """
+        fp_metadata = self.exp_path / "metadata_emptyrows.csv"
+        info = experiment.load_metadata(fp_metadata)
+        self.assertEqual(self.expected, info)
+
+    def test_load_metadata_emptycols(self):
+        """Test metadata loading including empty columns.
+
+        Empty columns should be silently removed.
+        """
+        fp_metadata = self.exp_path / "metadata_emptycols.csv"
+        info = experiment.load_metadata(fp_metadata)
+        self.assertEqual(self.expected, info)
